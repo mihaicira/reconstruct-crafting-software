0a9f2b2cb48669cbd97e0e550bcad65c1bbbfce3
mihaicira <70019764+mihaicira@users.noreply.github.com>
2021-12-14 14:50:37 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+function reconstruct(words,string){
+    /*
+    * @param words: list of words
+    * @param string: desired sentence without spaces
+    * **/
+    let wordBuild = ""
+    let final_sentence = [];
+    for(let i = 0 ; i < string.length ; i++){
+        wordBuild+= string[i]
+        if(words.includes(wordBuild)){
+            words.splice(words.indexOf(wordBuild),1)
+            final_sentence.push(wordBuild)
+            wordBuild = ""
+        }
+    }
+
+    if(wordBuild !== "")
+        return []
+
+    return final_sentence;
+}
+
+const compareArrays = (x,y) =>{
+    //true if params (arrays) have the same values, false otherwise
+    return Array.isArray(x) && Array.isArray(y) && x.length === y.length && x.every((val,index)=>val === y[index]);
+}
+
+function unitTest(tests){
+    tests.forEach((test,idx)=>{
+        let value = reconstruct(test.words,test.string)
+        let desired_result = test.desired
+        console.assert(compareArrays(value,desired_result),`expected: ${value} | got: ${desired_result} (test #${idx+1})`)
+    })
+    console.log("tests done")
+}
+
+unitTest([
+    {
+        words:['quick','brown','the','fox'],
+        string:'thequickbrownfox',
+        desired:['the','quick','brown','fox']
+    },
+    {
+        words:['bed','bath','bedbath','and','beyond'],
+        string:'',
+        desired:[]
+    },
+    {
+        words:['bed','bath','bedbath','and','beyond'],
+        string:'bedbathandbeyond',
+        desired:['bed', 'bath', 'and', 'beyond']
+    },
+    {
+        words:['0','bath','','and','beyond'],
+        string:'bathandbeyond0',
+        desired:["bath","and","beyond",'0']
+    },
+    {
+        words:[' ','.','!','?'],
+        string:'??',
+        desired:[]
+    },
+
+])
+
+
